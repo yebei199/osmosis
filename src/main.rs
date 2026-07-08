8451bed57a8c0c4ced354470caf6eca61b9f77d7
Sibyl <scauyb@qq.com>
2026-07-08 16:23:25 +0800

    style: apply rustfmt line wrapping

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 //! Build with: `cargo run --features desktop`
 
 fn main() {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    env_logger::Builder::from_env(
+        env_logger::Env::default()
+            .default_filter_or("info"),
+    )
+    .init();
     slint_study::run_app();
 }
